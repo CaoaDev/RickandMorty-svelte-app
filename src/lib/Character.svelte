565f4aff9ae6f3c0361e627a554b8ffd7d6756bb
CaoaDev <caoa_sinos9@outlook.es>
2022-08-29 02:18:54 -0500

    up

---
+++ src/lib/Character.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let character
+</script>
+
+<div class='character'>
+  <img src={character.image} alt={character.name} />
+  <h1>Personaje: {character.name}</h1>
+  <h3>Especie: {character.species}</h3>
+</div>
